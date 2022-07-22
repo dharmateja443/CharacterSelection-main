b4a9f7dee2bcb3ec00e7d47d87c0708c72f44dd5
dharmateja443 <dharmateja443@gmail.com>
2022-07-22 12:05:17 +0530

    Tank Spawnner Aded

---
+++ Assets/Scripts/TankSpawnner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawnner : MonoBehaviour
+{
+    [SerializeField] GameObject tanker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(tanker, transform.position, Quaternion.identity); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
